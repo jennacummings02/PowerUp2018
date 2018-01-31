64bac517f1620b6ca30e13cef837050c0cb3fc78
Rundle ThinkPad <srhigh@rundle.ab.ca>
2018-01-31 16:16:37 -0700

    Updated Driving Motors from Spark to VictorSP

--- PowerUp2018/src/org/usfirst/frc/team6351/robot/subsystems/DriveTrain.java
+++ PowerUp2018/src/org/usfirst/frc/team6351/robot/subsystems/DriveTrain.java
@@ -15,24 +15,26 @@ public class DriveTrain extends Subsystem {
     // here. Call these from Commands.
 	
 	//Left Motors
-//    VictorSP frontLeft = new VictorSP(1);
-    Spark Left = new Spark(2);
+    VictorSP frontLeft = new VictorSP(3);
+    VictorSP backLeft = new VictorSP(4);
     //Right Motors
-//    VictorSP frontRight = new VictorSP(3);
-    Spark Right = new Spark(1);
+    VictorSP frontRight = new VictorSP(1);
+    VictorSP backRight = new VictorSP(2);
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         setDefaultCommand(new GTADrive());
     }
     public void setLeft (double speed) {
-    		Left.set(speed);
+    		frontLeft.set(speed);
+    		backLeft.set(speed);
 //    		backLeft.set(speed);
     	
 
     }
     public void setRight (double speed) {
-    		Right.set(speed);
+    		frontRight.set(speed);
+    		backRight.set(speed);
 //    		backRight.set(speed);
     	
     }
